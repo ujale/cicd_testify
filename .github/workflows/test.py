dc649909ac09ffd0a6d0578515303f028146cce9
Udeme Jalekun <udemejalekun@Udemes-MacBook-Pro.local>
2022-09-11 16:28:35 +0100

    add unittest for python

---
+++ .github/workflows/test.py
@@ -0,0 +1,12 @@
+import main
+import unittest
+
+class TestMain(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(main.addition(1, 2), 3, "should be 3")
+        self.assertEqual(main.addition(5, 5), 10, "should be 10")
+        self.assertEqual(main.addition(-3, 2), -1, "should be -1")
+
+if __name__ == '__main__':
+    unittest.main()
